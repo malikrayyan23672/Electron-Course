35456d047d4fd5cde1b3732c351b30feaeb5f7ab
malikrayyan23672 <malikrayyan263@gmail.com>
2026-08-22 20:27:51 +0500

    feat: add EventPayloadMapping type for improved type safety in IPC communication

--- 1_electron_app/types.d.ts
+++ 1_electron_app/types.d.ts
@@ -10,6 +10,12 @@ type StaticData = {
     cpuModel: string;
 }
 
+type EventPayloadMapping = {
+
+    statistics: Statistics
+    getStaticData: StaticData;
+}
+
 interface Window{
 
     electron: {
