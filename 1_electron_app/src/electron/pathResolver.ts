e77137eb3dec7523a177ed77e74184a148a2ab74
malikrayyan23672 <malikrayyan263@gmail.com>
2026-08-22 21:27:34 +0500

    feat: add getUIPath function for resolving UI path in Electron app

--- 1_electron_app/src/electron/pathResolver.ts
+++ 1_electron_app/src/electron/pathResolver.ts
@@ -7,3 +7,7 @@ export function getPreloadPath(){
 
     return path.join(app.getAppPath(), isDev() ? '.' : '..', '/dist-electron/preload.cjs')
 }
+
+export function getUIPath(){
+    return path.join(app.getAppPath(), '/dist-react/index.html')
+}
